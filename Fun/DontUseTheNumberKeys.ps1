20fccd3498f25337647150980c745ec58151f68f
Mouseboy007 <66361777+Mouseboy007@users.noreply.github.com>
2020-07-18 10:18:49 +0100

    Create DontUseTheNumberKeys.ps1
    Just a silly demonstration of how you could generate numbers without resorting to the proper number buttons on a keyboard - note I don't even use a number in the code!

---
+++ Fun/DontUseTheNumberKeys.ps1
@@ -0,0 +1,35 @@
+﻿#############################################
+#                                           #
+#        DontUseTheNumberKeys.Ps1           #
+#      Just to demonstrate you can get      #
+#        numbers without using the          #
+#       number keys or number keypad        #
+#          Andy Pyne 2020.07.18             #
+#                                           #
+#############################################
+
+$One = $One.Length
+$Two = $One + $One
+$Three = $One + $Two
+
+$ZeroToNineArray = New-Object System.Collections.Arraylist
+
+$ZeroToNineArray.Add($One - $One)              #Zero
+$ZeroToNineArray.Add($One)                     #One
+$ZeroToNineArray.Add($Two)                     #Two
+$ZeroToNineArray.Add($Three)                   #Three
+$ZeroToNineArray.Add($Two * $Two)              #Four
+$ZeroToNineArray.Add($Three + $Two)            #Five
+$ZeroToNineArray.Add($Three * $Two)            #Six
+$ZeroToNineArray.Add($Three + $Two + $Two)     #Seven
+$ZeroToNineArray.Add([Math]::Pow($Two,$Three)) #Eight
+$ZeroToNineArray.Add([Math]::Pow($Three,$Two)) #$Nine
+
+Do {
+Clear
+$Digit = Get-Random -InputObject $ZeroToNineArray
+Write-Host $Digit "Copied to Clipboard"
+$Digit | Clip
+Write-Host
+Pause
+} Until ($Hell -eq "FreezesOver")
